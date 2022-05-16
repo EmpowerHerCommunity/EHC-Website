1d5973f80406b09df69ac1501188bb69d18febba
Emmanuel Onyeka Eboh <ecolejnr007@gmail.com>
2022-05-16 11:41:53 +0100

    first commit

---
+++ pages/contact.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import NavBar from '../src/components/NavBar';
+import Form from '../src/components/Form';
+import ContactUs from '../src/components/ContactUs';
+import Footer from '../src/components/Footer';
+
+const Contact = () => {
+  return (
+    <div>
+        Veedahv, this is your part of the work <b>  Contact Page</b> Do not bother with the NavBar and Footer components
+    <NavBar />
+    <Form />
+    <ContactUs />
+    <Footer />
+    </div>
+  )
+}
+
+export default Contact
---
+++ pages/volunteer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import NavBar from '../src/components/NavBar';
+import Hero from '../src/components/Hero';
+import Cards from '../src/components/Cards';
+import Footer from '../src/components/Footer';
+
+const Volunteer = () => {
+  return (
+    <div>
+        
+        Pearl this is your part of the job: <b>Volunteer Page</b>. Do not bother with the NavBar and Footer pages yet. 
+        <NavBar />
+        <Hero />
+        <Cards />
+        <Footer />
+    </div>
+  )
+}
+
+export default Volunteer
