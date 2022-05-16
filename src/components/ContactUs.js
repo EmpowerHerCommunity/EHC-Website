0f20404da384d0a780e268be2aeb7a5974c4cb66
Victoria Akpan <veedahv55@gmail.com>
2022-05-16 20:13:10 +0100

    tailwind setup

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 const ContactUs = () => {
   return (
-    <div>ContactUs</div>
+    <div>
+      <h1 className="text-4xl">Contact Us</h1>
+      <p className="text-2xl">
+      Ask us anything, we are always eager to answers any questions you may have. 
+        </p>
+    </div>
   )
 }
 
