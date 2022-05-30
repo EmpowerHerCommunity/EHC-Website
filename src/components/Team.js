7875093cad183957cc241e256054771081ef898e
Victoria Akpan <veedahv55@gmail.com>
2022-05-30 11:34:33 +0100

    feat: teams.js, newsletter.js implemented

--- src/components/Team.js
+++ src/components/Team.js
@@ -2,7 +2,209 @@ import React from 'react'
 
 const Team = () => {
   return (
-    <div>Team</div>
+    <section className="">
+      <div className='container mx-auto py-5 text-dark relative'>
+        <img src="/contact-dots.svg" alt="" className="absolute top-[20px] left-[5px] sm:left-[-5px] my-5" />
+        <h2 className="text-5xl text-center my-10 w-fit mx-auto relative">
+          Meet The Team
+          <img src="/sun.png" alt="" className="absolute top-[-10px] right-[-25px] w-14" />
+          {/* <img src="/sun.png" alt="" className="absolute top-[-5px] right-[0px]" /> */}
+        </h2>
+        <div className="flex flex-wrap justify-center gap-8 md:gap-20 my-10">
+          <div className="w-[220px] xl:w-[250px] text-center">
+            <div className="w-[220px] xl:w-[250px] h-[220px] xl:h-[250px] bg-primary bg-opacity-10 rounded-full flex items-end justify-center overflow-hidden">
+              <img src="/team-images/beth.png" alt="Elizabeth Okaome image" className="w-[80%]" />
+            </div>
+            <p className="text-lg">
+              Elizabeth Okaome
+            </p>
+            <p className="text-[#737373]">
+              Founder
+            </p>
+            <div className="flex justify-center items-center gap-6 mt-2">
+              <a href="http://" className="" target="_blank" rel="noopener noreferrer">
+                <img src="/social-media/twitter.png" alt="" className="" />
+              </a>
+              <a href="http://" className="" target="_blank" rel="noopener noreferrer">
+                <img src="/social-media/linkedin.png" alt="" className="" />
+              </a>
+            </div>
+
+          </div>
+          <div className="w-[220px] xl:w-[250px] text-center">
+            <div className="w-[220px] xl:w-[250px] h-[220px] xl:h-[250px] bg-primary bg-opacity-10 rounded-full flex items-end justify-center overflow-hidden">
+              <img src="/team-images/precious.png" alt="Precious Oriarewo image" className="w-[80%]" />
+            </div>
+            <p className="text-lg">
+              Precious Oriarewo
+            </p>
+            <p className="text-[#737373]">
+              Lead, Operations
+            </p>
+            <div className="flex justify-center items-center gap-6 mt-2">
+              <a href="http://" className="" target="_blank" rel="noopener noreferrer">
+                <img src="/social-media/twitter.png" alt="" className="" />
+              </a>
+              <a href="http://" className="" target="_blank" rel="noopener noreferrer">
+                <img src="/social-media/linkedin.png" alt="" className="" />
+              </a>
+            </div>
+          </div>
+          <div className="w-[220px] xl:w-[250px] text-center">
+            <div className="w-[220px] xl:w-[250px] h-[220px] xl:h-[250px] bg-primary bg-opacity-10 rounded-full flex items-end justify-center overflow-hidden">
+              <img src="/team-images/peace.png" alt="Peace Ngozi Okafor image" className="w-[85%]" />
+            </div>
+            <p className="text-lg">
+              Peace Ngozi Okafor
+            </p>
+            <p className="text-[#737373]">
+              Lead, Community Manager
+            </p>
+            <div className="flex justify-center items-center gap-6 mt-2">
+              <a href="http://" className="" target="_blank" rel="noopener noreferrer">
+                <img src="/social-media/twitter.png" alt="" className="" />
+              </a>
+              <a href="http://" className="" target="_blank" rel="noopener noreferrer">
+                <img src="/social-media/linkedin.png" alt="" className="" />
+              </a>
+            </div>
+          </div>
+          <div className="w-[220px] xl:w-[250px] text-center">
+            <div className="w-[220px] xl:w-[250px] h-[220px] xl:h-[250px] bg-primary bg-opacity-10 rounded-full flex items-end justify-center overflow-hidden">
+              <img src="/team-images/tracy.png" alt="Tracy Obakhena image" className="w-[80%]" />
+            </div>
+            <p className="text-lg">
+              Tracy Obakhena
+            </p>
+            <p className="text-[#737373]">
+              Programs Manager
+            </p>
+            <div className="flex justify-center items-center gap-6 mt-2">
+              <a href="http://" className="" target="_blank" rel="noopener noreferrer">
+                <img src="/social-media/twitter.png" alt="" className="" />
+              </a>
+              <a href="http://" className="" target="_blank" rel="noopener noreferrer">
+                <img src="/social-media/linkedin.png" alt="" className="" />
+              </a>
+            </div>
+          </div>
+          <div className="w-[220px] xl:w-[250px] text-center">
+            <div className="w-[220px] xl:w-[250px] h-[220px] xl:h-[250px] bg-primary bg-opacity-10 rounded-full flex items-end justify-center overflow-hidden">
+              <img src="/team-images/mary.png" alt="Mary Sule image" className="w-[80%]" />
+            </div>
+            <p className="text-lg">
+              Mary Sule
+            </p>
+            <p className="text-[#737373]">
+              Lead, Finance
+            </p>
+            <div className="flex justify-center items-center gap-6 mt-2">
+              <a href="http://" className="" target="_blank" rel="noopener noreferrer">
+                <img src="/social-media/twitter.png" alt="" className="" />
+              </a>
+              <a href="http://" className="" target="_blank" rel="noopener noreferrer">
+                <img src="/social-media/linkedin.png" alt="" className="" />
+              </a>
+            </div>
+          </div>
+          <div className="w-[220px] xl:w-[250px] text-center">
+            <div className="w-[220px] xl:w-[250px] h-[220px] xl:h-[250px] bg-primary bg-opacity-10 rounded-full flex items-end justify-center overflow-hidden">
+              <img src="/team-images/pearl.png" alt="Pearl Christain image" className="w-[80%]" />
+            </div>
+            <p className="text-lg">
+              Pearl Christain
+            </p>
+            <p className="text-[#737373]">
+              Lead, Engineering
+            </p>
+            <div className="flex justify-center items-center gap-6 mt-2">
+              <a href="http://" className="" target="_blank" rel="noopener noreferrer">
+                <img src="/social-media/twitter.png" alt="" className="" />
+              </a>
+              <a href="http://" className="" target="_blank" rel="noopener noreferrer">
+                <img src="/social-media/linkedin.png" alt="" className="" />
+              </a>
+            </div>
+          </div>
+          <div className="w-[220px] xl:w-[250px] text-center">
+            <div className="w-[220px] xl:w-[250px] h-[220px] xl:h-[250px] bg-primary bg-opacity-10 rounded-full flex items-end justify-center overflow-hidden">
+              <img src="/team-images/kelechi.png" alt="Kelechi Amachukwu image" className="w-[85%]" />
+            </div>
+            <p className="text-lg">
+              Kelechi Amachukwu
+            </p>
+            <p className="text-[#737373]">
+              Lead, Design
+            </p>
+            <div className="flex justify-center items-center gap-6 mt-2">
+              <a href="http://" className="" target="_blank" rel="noopener noreferrer">
+                <img src="/social-media/twitter.png" alt="" className="" />
+              </a>
+              <a href="http://" className="" target="_blank" rel="noopener noreferrer">
+                <img src="/social-media/linkedin.png" alt="" className="" />
+              </a>
+            </div>
+          </div>
+          <div className="w-[220px] xl:w-[250px] text-center">
+            <div className="w-[220px] xl:w-[250px] h-[220px] xl:h-[250px] bg-primary bg-opacity-10 rounded-full flex items-end justify-center overflow-hidden">
+              <img src="/team-images/anita.png" alt="Anita Kimemenihia image" className="w-[80%]" />
+            </div>
+            <p className="text-lg">
+              Anita Kimemenihia
+            </p>
+            <p className="text-[#737373]">
+              Lead, Communications
+            </p>
+            <div className="flex justify-center items-center gap-6 mt-2">
+              <a href="http://" className="" target="_blank" rel="noopener noreferrer">
+                <img src="/social-media/twitter.png" alt="" className="" />
+              </a>
+              <a href="http://" className="" target="_blank" rel="noopener noreferrer">
+                <img src="/social-media/linkedin.png" alt="" className="" />
+              </a>
+            </div>
+          </div>
+          <div className="w-[220px] xl:w-[250px] text-center">
+            <div className="w-[220px] xl:w-[250px] h-[220px] xl:h-[250px] bg-primary bg-opacity-10 rounded-full flex items-end justify-center overflow-hidden">
+              <img src="/team-images/gracious.png" alt="Gracious Sede image" className="w-[80%]" />
+            </div>
+            <p className="text-lg">
+              Gracious Sede
+            </p>
+            <p className="text-[#737373]">
+              Legal Advisor
+            </p>
+            <div className="flex justify-center items-center gap-6 mt-2">
+              <a href="http://" className="" target="_blank" rel="noopener noreferrer">
+                <img src="/social-media/twitter.png" alt="" className="" />
+              </a>
+              <a href="http://" className="" target="_blank" rel="noopener noreferrer">
+                <img src="/social-media/linkedin.png" alt="" className="" />
+              </a>
+            </div>
+          </div>
+          <div className="w-[220px] xl:w-[250px] text-center">
+            <div className="w-[220px] xl:w-[250px] h-[220px] xl:h-[250px] bg-primary bg-opacity-10 rounded-full flex items-end justify-center overflow-hidden">
+              <img src="/team-images/eunice.png" alt="Eunice Kadri image" className="w-[85%]" />
+            </div>
+            <p className="text-lg">
+              Eunice Kadri
+            </p>
+            <p className="text-[#737373]">
+              Public Relations Officer
+            </p>
+            <div className="flex justify-center items-center gap-6 mt-2">
+              <a href="http://" className="" target="_blank" rel="noopener noreferrer">
+                <img src="/social-media/twitter.png" alt="" className="" />
+              </a>
+              <a href="http://" className="" target="_blank" rel="noopener noreferrer">
+                <img src="/social-media/linkedin.png" alt="" className="" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
