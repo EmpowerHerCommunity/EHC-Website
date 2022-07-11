9438f3a9c3e829b9151eeee181a443b477d8ac74
Victoria Akpan <veedahv55@gmail.com>
2022-07-11 22:34:20 +0100

    Volunteer page ui fix

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -15,14 +15,33 @@ const Hero = () => {
 				<div className={styles.hero}>
 					<div className='flex justify-around gap-24'>
 						<div className='hidden md:block h-2/4 lg:w-2/5 lg:flex-auto' data-aos='fade-up'>
-						<div className='bg-slate-100 rounded-r-lg w-fit mx-auto' data-aos='fade-up'>
-						{/* <div className='bg-slate-100 rounded-r-lg h-2/4' data-aos='fade-up'> */}
-							<Image src='/cup.png' className='object-cover' width='300px' height='339px' alt='' />
-						</div>
+                        {/* <div className="relative p-4"> */}
+                            {/* <div className="flex absolute top-0 left-0">
+                                <Image src='/border-tl.png' width='106px' height='106px' alt='' />
+                            </div>
+                            <div className="flex absolute top-0 right-0">
+                                <Image src='/border-tr.png' width='106px' height='106px' alt='' />
+                            </div>
+                            <div className="flex absolute bottom-0 left-0">
+                                <Image src='/border-bl.png' width='106px' height='106px' alt='' />
+                            </div>
+                            <div className="flex absolute bottom-0 right-0">
+                                <Image src='/border-br.png' width='106px' height='106px' alt='' />
+                            </div>
+                            <div className='relative overflow-hidden rounded-bl-[4rem] rounded-tr-[3rem] flex w-full max-w-max' data-aos='fade-right'> */}
+                                {/* <Image src='/herogirl.png' width='548px' height='539px' alt='' /> */}
+                                {/* <Image src={event.image} width='732px' height='744px' alt={event.title} /> */}
+								{/* <Image src='/volunteers.JPEG' className='object-cover' width='384px' height='384px' alt='' /> */}
+                            {/* </div> */}
+                        {/* </div> */}
+							<div className='w-fit mx-auto' data-aos='fade-up'>
+								{/* <div className='bg-slate-100 rounded-r-lg h-2/4' data-aos='fade-up'> */}
+								<Image src='/volunteers.JPEG' className='object-cover' width='384px' height='384px' alt='' />
+							</div>
 						</div>
 
 						<div className='lg:w-3/5 lg:flex-auto' data-aos='fade-left'>
-						{/* <div data-aos='fade-left'> */}
+							{/* <div data-aos='fade-left'> */}
 							<article>
 								<div className='hero-h2 md:text-6xl  text-5xl font-bold'>
 									Call For Volunteers
