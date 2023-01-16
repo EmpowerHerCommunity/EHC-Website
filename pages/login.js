eb065e576aefe0654b9ed2d8e2dc3a71e56d78f1
Juliet <julieto2001@gmail.com>
2023-01-16 21:33:47 +0100

    login page

--- pages/login.js
+++ pages/login.js
@@ -6,29 +6,29 @@ const login = () => {
     return (
         <>
             <NavBar />
-            <div className="flex flex-col justify-center h-screen">
+            <section className="flex flex-col justify-center h-screen">
                 <div className="lg:max-w-xl w-full p-6 m-auto bg-white rounded-md shadow-2xl">
                     <h1 className="login text-4xl text-center font-semibold">
                         Log in
                     </h1>
                     <form className="mt-8">
                         <div>
-                        <label className="block">
-                             <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-bold text-slate-700">
-                              Email
-                              </span>
-                              <input type="email" name="email" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="you@example.com" />
-                          </label>
+                            <label className="block">
+                                <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-bold text-slate-700">
+                                    Email
+                                </span>
+                                <input type="email" name="email" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="you@example.com" />
+                            </label>
                         </div>
-                        
-                         <div>
-                             <label className="block py-8">
-                             <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-bold text-slate-700">
-                              Password
-                              </span>
-                              <input type="password" name="password" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" />
-                          </label>
-                         </div>
+
+                        <div>
+                            <label className="block py-8">
+                                <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-bold text-slate-700">
+                                    Password
+                                </span>
+                                <input type="password" name="password" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" />
+                            </label>
+                        </div>
 
                         <div className="flex flex-wrap justify-between mt-4">
                             <div>
@@ -50,7 +50,7 @@ const login = () => {
                         <a href="/" className="sign-up font-medium hover:underline"> Sign up </a>
                     </p>
                 </div>
-            </div>
+            </section>
             <Footer />
         </>
     )
