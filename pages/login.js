24ad4b3a659bbcaa4db5d2bc15b337c565198bca
Nancy <olatujamn.19@student.funaab.edu.ng>
2023-01-16 08:11:48 +0100

    Email and Password input

--- pages/login.js
+++ pages/login.js
@@ -12,6 +12,25 @@ const login = () => {
                         Log in
                     </h1>
                     <form className="mt-8">
+                        <div>
+                        <label className="block">
+                             <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-bold text-slate-700">
+                              Email
+                              </span>
+                              <input type="email" name="email" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="you@example.com" />
+                          </label>
+                        </div>
+                        <label className="block py-8">
+                             <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-bold text-slate-700">
+                              Password
+                              </span>
+                              <input type="password" name="password" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" />
+                          </label>
+                         <div>
+
+
+                         </div>
+
                         <div className="flex flex-wrap justify-between mt-4">
                             <div>
                                 <input type="checkbox" className="" />
