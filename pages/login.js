c8896f1d532414ba4ec9322042e6604f7ba75f08
Nancy <olatujamn.19@student.funaab.edu.ng>
2023-01-16 08:33:06 +0100

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -20,15 +20,14 @@ const login = () => {
                               <input type="email" name="email" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="you@example.com" />
                           </label>
                         </div>
-                        <label className="block py-8">
+                        
+                         <div>
+                             <label className="block py-8">
                              <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-bold text-slate-700">
                               Password
                               </span>
                               <input type="password" name="password" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" />
                           </label>
-                         <div>
-
-
                          </div>
 
                         <div className="flex flex-wrap justify-between mt-4">
