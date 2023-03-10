486eb2732e337328b666ba2c7ec45a7ea4e65e87
jessica <jessicajoseph2019@yahoo.com>
2023-03-10 18:06:12 +0100

    blog page code review and fix

--- pages/blog.js
+++ pages/blog.js
@@ -1,85 +1,15 @@
 // import "./App.css";
 import React from "react";
 import { useEffect, useState, useCallback } from "react";
-import NavBar from '../src/components/NavBar';
-import Footer from '../src/components/Footer';
-import BlogCard from '../src/components/BlogCard';
+import NavBar from "../src/components/NavBar";
+import Footer from "../src/components/Footer";
+import BlogCard from "../src/components/BlogCard";
 
 const App = () => {
-  const variables = { page: 0 };
-
-  const query = `
-        query GetUserArticles($page: Int!) {
-            user(username: "forjessicasake") {
-                publication {
-                    posts(page: $page) {
-                        title
-                        brief
-                        slug
-                        coverImage
-                    }
-                }
-            }
-        }
-`;
-
-  const fetchData = async () => {
-    const data = await fetch("https://api.hashnode.com/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        query,
-        variables,
-      }),
-    });
-    const result = await data.json();
-    const post = result.data.user.publication.posts;
-    setPosts(post);
-    setLoading(false);
-  };
-
-  const memoedFunction = useCallback(() => {
-    return fetchData();
-  }, [fetchData]);
-
-  useEffect(() => {
-    memoedFunction();
-  }, []);
-
-  const [posts, setPosts] = useState(null);
-  const [loading, setLoading] = useState(true);
-  console.log(posts);
   return (
     <div className="overflow-hidden">
       <NavBar />
-      <main className="main bg-light">
-        <div className="container xl:max-w-screen-xl px-4 mx-auto py-24 relative">
-          <section className="App font-mono">
-            {loading && <p className="ml-6">fetching blogs...</p>}
-
-                        {posts &&
-                            posts.map((post) => (
-                                <article key={post.id} className="flex justify-between ml-6 mr-4 mb-6 p-4 items-center text-justify cursor-pointer hover:shadow-2xl shadow-lg">
-                                    <a href={`https://blog.empowerher.community/${post.slug}`} target="_blank" rel="noopener noreferrer">
-                                        <img src={post.coverImage} alt="blog post cover" className="lg:w-96 w-36" />
-                                    </a>
-                                    <a href={`https://blog.empowerher.community/${post.slug}`} target="_blank" rel="noopener noreferrer">
-                                        <figcaption className="ml-6">
-                                            <h2 className="font-bold lg:text-lg text-sm mb-2">{post.title}</h2>
-                                            <p className="lg:text-base text-xs">{post.brief}</p>
-                                        </figcaption>
-                                    </a>
-                                </article>
-                            ))}
-                    </section>
-                </div>
-            </main>
-            <BlogCard />
-            <Footer />
-        </div>
-      </main>
+      <BlogCard />
       <Footer />
     </div>
   );
--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -1,56 +1,65 @@
-import React from 'react'
+const blogs = [
+  {
+    date: "4 March 2023",
+    read: "5 Minutes Read",
+    image: "/blog-card.jpg",
+    title: "Why CSS units matter to your responsive website designs",
+    description:
+      "Let's discuss using CSS units to create fully responsive websites that size and resize elements. Plus, how to decide on the best units for each situation.",
+    author: "Aniekwe Jessica",
+    tag: ["CSS", "React"],
+  },
+];
 
 const BlogCard = () => {
-    return (
-        <>
-            <section>
-                <section className="container px-5 py-16 mx-auto">
-                    <div className="flex flex-wrap -m-4">
-                        <article className="xl:w-1/2 md:w-1/2 p-4">
-                            <div className="border-2 border-black px-3 py-2 rounded-lg">
-                                <img className="h-64 rounded-lg w-full object-cover object-center mb-6" src="/blog-card.jpg" alt="content" />
-                                <section className="flex">
-                                    <h2 className="text-xs font-medium mb-1">4 March 2023 </h2>
-                                    <h2 className="mb-1 ml-2 text-xs"> . 5 Minutes Read</h2>
-                                </section>
-                                <h1 className="lg:text-2xl text-xl font-bold mt-1 mb-2">Why CSS units matter to your responsive website designs</h1>
-                                <p className="text-base leading-relaxed">Let's discuss using CSS units to create fully responsive websites that size and resize elements. Plus, how to decide on the best units for each situation.</p>
-                                <section className="flex items-center flex-wrap mt-3">
-                                    <img src="/blog-card.jpg" className="w-8 h-8 rounded-full" alt="writer" />
-                                    <p className="font-medium ml-2 md:mb-1 lg:mb-0">Aniekwe Jessica</p>
-                                </section>
-                                <aside className="flex items-center flex-wrap mb-2 mt-4">
-                                    <button className="rounded-2xl border-2 border-black py-1 px-4">CSS</button>
-                                    <button className="rounded-2xl border-2 border-black py-1 px-4 ml-2">React</button>
-                                </aside>
-                            </div>
-                        </article>
+  return (
+    <>
+      <section>
+        <section className="container px-5 py-16 mx-auto">
+          {blogs &&
+            blogs.map((blog) => (
+              <article className="xl:w-1/2 md:w-1/2 p-4">
+                <div className="border-2 border-black px-3 py-2 rounded-lg">
+                  <img
+                    className="h-64 rounded-lg w-full object-cover object-center mb-6"
+                    src={blog.image}
+                    alt="content"
+                  />
+                  <section className="flex">
+                    <h2 className="text-xs font-medium mb-1">{blog.date}</h2>
+                    <h2 className="mb-1 ml-2 text-xs">{blog.read}</h2>
+                  </section>
+                  <h1 className="lg:text-2xl text-xl font-bold mt-1 mb-2">
+                    {blog.title}
+                  </h1>
+                  <p className="text-base leading-relaxed">
+                    {blog.description}
+                  </p>
+                  <section className="flex items-center flex-wrap mt-3">
+                    <img
+                      src="/blog-card.jpg"
+                      className="w-8 h-8 rounded-full"
+                      alt="writer"
+                    />
+                    <p className="font-medium ml-2 md:mb-1 lg:mb-0">
+                      {blog.author}
+                    </p>
+                  </section>
+                  <aside className="flex items-center flex-wrap mb-2 mt-4">
+                    <button className="rounded-2xl border-2 border-black py-1 px-4">
+                      {blog.tag[0]}
+                    </button>
+                    <button className="rounded-2xl border-2 border-black py-1 px-4 ml-2">
+                      {blog.tag[1]}
+                    </button>
+                  </aside>
+                </div>
+              </article>
+            ))}
+        </section>
+      </section>
+    </>
+  );
+};
 
-                        <article className="xl:w-1/2 md:w-1/2 p-4">
-                            <div className="border-2 border-black px-3 py-2 rounded-lg">
-                                <img className="h-64 rounded-lg w-full object-cover object-center mb-6" src="/blog-card.jpg" alt="content" />
-                                <section className="flex">
-                                    <h2 className="text-xs font-medium mb-1">4 March 2023 </h2>
-                                    <h2 className="mb-1 ml-2 text-xs"> . 5 Minutes Read</h2>
-                                </section>
-                                <h1 className="lg:text-2xl text-xl font-bold mt-1 mb-2">Why CSS units matter to your responsive website designs</h1>
-                                <p className="leading-relaxed text-base">Let's discuss using CSS units to create fully responsive websites that size and resize elements. Plus, how to decide on the best units for each situation.</p>
-                                <section className="flex items-center flex-wrap mt-3">
-                                    <img src="/blog-card.jpg" className="w-8 h-8 rounded-full" alt="writer" />
-                                    <p className="font-medium ml-2 md:mb-1 lg:mb-0">Aniekwe Jessica</p>
-                                </section>
-                                <aside className="flex items-center flex-wrap mb-2 mt-4">
-                                    <button className="rounded-2xl border-2 border-black py-1 px-4">CSS</button>
-                                    <button className="rounded-2xl border-2 border-black py-1 px-4 ml-2">React</button>
-                                </aside>
-                            </div>
-                        </article>
-                       
-                    </div>
-                </section>
-            </section>
-        </>
-    )
-}
-
-export default BlogCard
+export default BlogCard;
