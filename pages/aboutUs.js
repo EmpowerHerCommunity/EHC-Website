ff450fbbd510b6d1ff8373389d4008b9fb116e65
jessica <jessicajoseph2019@yahoo.com>
2023-03-15 14:23:54 +0100

    feat:popUp page up

--- pages/aboutUs.js
+++ pages/aboutUs.js
@@ -5,14 +5,12 @@ import OurVision from '../src/components/OurVision';
 import Team from '../src/components/Team';
 import NewsLetter from '../src/components/NewsLetter';
 import Footer from '../src/components/Footer'
-import PopUp from '../src/components/Popup';
 
 const aboutUs = () => {
   return (
     <div className="overflow-hidden">
       <NavBar />
       <main className="main overflow-hidden">
-        <PopUp/>
         <HeroAbout />
         <OurVision />
         <Team />
