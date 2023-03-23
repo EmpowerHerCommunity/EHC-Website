5c81eae48a3dc7e435c7b72243ea76b2885847a6
jessica <jessicajoseph2019@yahoo.com>
2023-03-23 16:27:46 +0100

    fix:bog UI feature page

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -45,38 +45,41 @@ const Slug = () => {
   return (
     <section className="container mx-auto">
       <NavBar />
-      <div className="prose lg:prose-lg prose-lg max-w-screen-xl text-justify px-14 lg:px-28 pt-6 lg:pt-14">
+      <div className="prose lg:prose-xl prose-xl max-w-screen-xl text-justify px-14 lg:px-28 pt-6 lg:pt-14">
         {blogs &&
           blogs
             .filter((blog) => blog.slug === routeId)
             .map((blog) => (
               <article key={blog.slug} className="py-6">
-                <section className=" text-slug flex items-center justify-between w-64 lg:text-xl text-2xl">
+                <section className=" text-slug flex items-center justify-between w-96 lg:text-xl text-2xl">
                   <div className=" font-medium mb-1">
                     {new Date(blog.created).toLocaleDateString(
                       "en-US",
                       options
                     )}
                   </div>
+                  <div className="border-black h-1 w-1 bg-black rounded-full"></div>
+
                   <div className="-mt-1">
                     {`${calculateReadingTime(blog.description)}` > 1
                       ? `${calculateReadingTime(blog.description)} minutes read`
                       : `${calculateReadingTime(blog.description)} minute read`}
                   </div>
                 </section>
+
                 <figcaption>
-                  <h1 className="lg:text-4xl text-4xl md-96 lg:w-12/12 font-semibold">{blog.title}</h1>
-                  <p className="md:text-lg text-xl lg:text-lg lg:w-9/12">{blog.introduction}</p>
+                  <h1 className="lg:text-4xl text-5xl md-96 lg:w-12/12 font-semibold">{blog.title}</h1>
+                  <p className="md:text-lg text-2xl lg:text-lg lg:w-9/12">{blog.introduction}</p>
                 </figcaption>
 
-                <section className="flex items-center -mt-8 py-0 ">
+                <section className="flex items-center -mt-10 py-0 ">
                   <figure className="">
                     <img
                       src={blog.author_image}
                       className="rounded-full h-10 w-10"
                     />
                   </figure>
-                  <figcaption className="text-xl ml-6">{blog.author}</figcaption>
+                  <figcaption className="text-2xl ml-6">{blog.author}</figcaption>
                 </section>
 
                 <figure className="-mt-1">
