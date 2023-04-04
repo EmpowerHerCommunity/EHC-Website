0cc84168956d24729a06726e34a92125f9b3ce62
jessica <jessicajoseph2019@yahoo.com>
2023-04-04 17:27:48 +0100

    fix: blog fix

--- src/components/UpcomingEventsCard.js
+++ src/components/UpcomingEventsCard.js
@@ -1,9 +1,7 @@
 import React, { useEffect, useState } from "react";
-import Image from "next/image";
 import Link from "next/link";
 import AOS from "aos";
 import { BsCalendarFill } from "react-icons/bs";
-import { MdLocationPin } from "react-icons/md";
 
 const UpcomingEventsCard = ({ events }) => {
   useEffect(() => {
