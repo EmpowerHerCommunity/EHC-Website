cbae98b7c251ebe022d2d7e6ce4f999b75774ca8
jessica <jessicajoseph2019@yahoo.com>
2023-04-05 21:30:21 +0100

    feat: pagination fixed

--- src/components/Post.js/Post.js
+++ src/components/Post.js/Post.js
@@ -4,12 +4,12 @@ import BlogCard from "../Blog/BlogCard";
 import BlogNewsletter from "../Blog/BlogNewsletter";
 import Featured from "../Blog/Featured";
 
-const post = ({ blogs, isFetching }) => {
+const post = ({ blogs, isFetching, currentPage, totalPages}) => {
   return (
     <section>
       <NavBar />
       <Featured/>
-      <BlogCard blogs = {blogs} isFetching={isFetching} />
+      <BlogCard blogs = {blogs} isFetching={isFetching}  currentPage={currentPage} totalPages={totalPages} />
       <BlogNewsletter />
       <Footer />
     </section>
