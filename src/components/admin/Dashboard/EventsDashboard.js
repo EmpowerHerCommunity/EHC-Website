571a607f3b59dde6d7e045d6cca68cb429bd51d6
jessica <jessicajoseph2019@yahoo.com>
2023-04-07 10:31:52 +0100

    fix: navbar

--- src/components/admin/Dashboard/EventsDashboard.js
+++ src/components/admin/Dashboard/EventsDashboard.js
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { useRouter } from "next/router";
 
 const EventDashboard = () => {
   const [fetchedEvents, setFetchedEvents] = useState(null);
