e80cd7ad2ab20f81cd42d149d541884344691ae3
jessica <jessicajoseph2019@yahoo.com>
2023-05-06 14:51:37 +0100

    feat: connected email service to support page

--- src/components/Support.js
+++ src/components/Support.js
@@ -1,12 +1,73 @@
 import Image from "next/image";
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
+import { useRouter } from "next/router";
 import AOS from "aos";
+import emailjs from "@emailjs/browser";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Support = () => {
   useEffect(() => {
     AOS.init({ duration: 2500 });
     AOS.refresh();
   }, []);
+
+  const [isloading, setIsLoading] = React.useState(false);
+  const router = useRouter();
+  const [mail, setMail] = React.useState({
+    email: false,
+    laptops: false,
+    courses: false,
+    stipends: false,
+    others: false,
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (
+      (submit.current && mail.email) ||
+      mail.laptops ||
+      mail.courses ||
+      mail.stipends ||
+      mail.others
+    ) {
+      try {
+        setIsLoading(true);
+        await emailjs.sendForm(
+          "service_sqhbqh5",
+          "template_h207e6g",
+          submit.current,
+          "v_22EFbmmIAh7jydz"
+        );
+        toast.success("mail sent successfully", {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+        setIsLoading(false);
+        router.reload(); // This will refresh the page after the form is submitted
+      } catch (error) {
+        setIsLoading(false);
+        toast.error("Network error, please try again.", {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+      }
+    }
+  };
+
+  const submit = useRef(null);
 
   return (
     <section className="mx-auto mb-5 container flex items-center px-4 flex-col justify-center mt-8">
@@ -50,33 +111,75 @@ const Support = () => {
         <p className="lg:text-lg text-base text-blogSearch">
           Choose Materials to donate:
         </p>
-        <form className="flex flex-col text-lg mt-2 cursor-pointer">
+        <form
+          className="flex flex-col text-lg mt-2 cursor-pointer"
+          onSubmit={handleSubmit}
+          ref={submit}
+        >
           <label className="mb-4 flex items-center">
-            <input className="mr-2 h-5 w-5" type="checkbox"></input>
+            <input
+              className="mr-2 h-5 w-5"
+              type="checkbox"
+              name="from_laptops"
+              id="laptops"
+              onChange={(e) => setMail({ ...mail, laptops:true})}
+            />
             Laptops
           </label>
           <label className="mb-4 flex items-center">
-            <input className="mr-2 h-5 w-5" type="checkbox"></input>
+            <input
+              className="mr-2 h-5 w-5"
+              type="checkbox"
+              name="form_paidcourses"
+              id="courses"
+              onChange={(e) => setMail({ ...mail, courses:true})}
+            />
             Paid Courses
           </label>
           <label className="mb-4 flex items-center">
-            <input className="mr-2 h-5 w-5" type="checkbox"></input>
+            <input
+              className="mr-2 h-5 w-5"
+              type="checkbox"
+              name="form_stipends"
+              id="stipends"
+              onChange={(e) => setMail({ ...mail, stipends:true})}
+            />
             Data Stipends
           </label>
           <label className="mb-4 flex items-center">
-            <input className="mr-2 h-5 w-5" type="checkbox"></input>
+            <input
+              className="mr-2 h-5 w-5"
+              type="checkbox"
+              name="form_others"
+              id="others"
+              onChange={(e) => setMail({ ...mail, others:true})}
+            />
             Others
           </label>
-
           <input
             placeholder="Enter your email"
             className="placeholder:text-xl border-2 px-4 py-2 mt-2 w-96 h-14 rounded-xl"
-          ></input>
+            name="form_email"
+            id="email"
+            onChange={(e) => setMail({ ...mail, email: e.target.value })}
+          />
           <button className="bg-payment mt-6 text-white w-full border text-xl h-14 rounded-full">
-            Donate Selected
+            {isloading ? "Donate Selected" : "Your request is being sent"}
           </button>
         </form>
       </div>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </section>
   );
 };
