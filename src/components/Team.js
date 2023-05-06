0c4b7e12e1161e6c2f2efc7800bc70425a86b1f6
Nancy <olatujamn.19@student.funaab.edu.ng>
2023-05-06 12:10:46 +0100

    Update Team.js

--- src/components/Team.js
+++ src/components/Team.js
@@ -166,10 +166,10 @@ const Team = () => {
               Lead, Communications
             </p>
             <div className="flex justify-center items-center gap-6 mt-2">
-              <a href="http://www.linkedin.com/in/anita-kimemenihia" className="" target="_blank" rel="noopener noreferrer">
+              <a href="https://twitter.com/Restless_kim?s=0" className="" target="_blank" rel="noopener noreferrer">
                 <img src="/social-media/twitter.png" alt="" className="" />
               </a>
-              <a href="https://twitter.com/Restless_kim?s=09" className="" target="_blank" rel="noopener noreferrer">
+              <a href="http://www.linkedin.com/in/anita-kimemenihia" className="" target="_blank" rel="noopener noreferrer">
                 <img src="/social-media/linkedin.png" alt="" className="" />
               </a>
             </div>
