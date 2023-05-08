b237ecab9e55f9761d4bd0349af536d427805cd7
jessica <jessicajoseph2019@yahoo.com>
2023-05-08 21:37:49 +0100

    feat:card reference

--- pages/support.js
+++ pages/support.js
@@ -2,11 +2,13 @@ import NavBar from "../src/components/NavBar";
 import Support from "../src/components/Support";
 import Footer from "../src/components/Footer"
 import SupportPartnership from "../src/components/SupportPartnership";
+import SupportHero from "../src/components/SupportHero";
 
 const support = () => {
   return (
     <div className="overflow-y-hidden">
       <NavBar />
+      <SupportHero/>
       <SupportPartnership/>
       <Support />
       <Footer/>
