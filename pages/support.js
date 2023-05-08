47f9d117428adb655a061cbc433baa403810ef87
Juliet Oma <julieto2001@gmail.com>
2023-05-08 20:56:17 +0100

    support page hero section

--- pages/support.js
+++ pages/support.js
@@ -1,11 +1,13 @@
 import NavBar from "../src/components/NavBar";
 import Support from "../src/components/Support";
 import Footer from "../src/components/Footer"
+import SupportHero from "../src/components/SupportHero";
 
 const support = () => {
   return (
     <div className="overflow-y-hidden">
       <NavBar />
+      <SupportHero />
       <Support />
       <Footer/>
     </div>
